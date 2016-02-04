cf13650ef10f1f65eaf4f22e5114285881e21b34
Moez Bouhlel <bmoez.j@gmail.com>
2016-02-05 00:21:13 +0100

    update Member schema add test for Member

--- db/models.py
+++ db/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Member(models.Model):
     name = models.CharField(max_length=40)
     family_name = models.CharField(max_length=40)
-    birthday = models.DateField(blank=True)
+    birthday = models.DateField(blank=True, null=True)
     course = models.CharField(max_length=10)
     phone = models.CommaSeparatedIntegerField(max_length=20)
-    address = models.CharField(max_length=400, blank=True)
+    address = models.CharField(max_length=400, blank=True, null=True)
     email = models.EmailField()
-    new = models.BooleanField()
-    confirmed = models.BooleanField()
-    dreamspark_key = models.BooleanField()
-    member_card = models.BooleanField()
+    new = models.BooleanField(default=True)
+    confirmed = models.BooleanField(default=False)
+    dreamspark_key = models.BooleanField(default=False)
+    member_card = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s %s" % (self.name, self.family_name)
