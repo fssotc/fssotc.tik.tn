e1a930838045cb2c99cec09790e2afb8fa0e39a3
Moez Bouhlel <bmoez.j@gmail.com>
2016-02-09 11:01:37 +0100

    add rss feed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.contrib.syndication.views import Feed
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
+from django.core.urlresolvers import reverse
 from wordpress.models import Post
 
 class PostDetail(DetailView):
@@ -11,3 +13,25 @@ class PostList(ListView):
     template_name = 'blog/index.html'
     ordering = '-modified'
     paginate_by = 5
+
+class PostFeed(Feed):
+    title = "MTCFSS News"
+    link = "/blog/"
+
+    def items(self):
+        return Post.objects.order_by('-post_date')[:20]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return reverse('blog:post', kwargs={"pk": item.pk})
+
+    def item_pubdate(self, item):
+        return item.post_date
+
+    def item_author_name(self, item):
+        return item.author.name
