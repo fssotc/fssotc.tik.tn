3f1f36963ee1413cc17c6f7f69264f0d635470e4
Moez Bouhlel <bmoez.j@gmail.com>
2016-10-22 22:20:17 +0100

    only show confirmed members

--- website/views.py
+++ website/views.py
@@ -21,9 +21,11 @@ class MemberList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        members_insc = self.model.objects.filter(role__exact='').order_by('member__name', 'member__family_name')
+        members_insc = self.model.objects.filter(role__exact='', confirmed=True)\
+            .order_by('member__name', 'member__family_name')
         context['member_list'] = [insc for insc in members_insc if insc.is_current()]
-        admin_insc = self.model.objects.exclude(role__exact='').order_by('role', 'member__name', 'member__family_name')
+        admin_insc = self.model.objects.exclude(role__exact='')\
+            .order_by('role', 'member__name', 'member__family_name')
         context['admin_list'] = [insc for insc in admin_insc if insc.is_current()]
         return context
 
