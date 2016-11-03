68b3adf55a549c0be21de0979c67d1c7af3982b1
Moez Bouhlel <bmoez.j@gmail.com>
2016-11-03 12:08:35 +0100

    add template support to send_mail

--- db/forms.py
+++ db/forms.py
@@ -1,18 +1,43 @@
 from django import forms
 from django.core.mail import send_mass_mail
+from django.template import Context, Template
 import django.conf
 import threading
+
+from .models import Member
 
 
 class MassMailThread(threading.Thread):
 
-    def __init__(self, mails, fail_silently=True):
-        self.mails = mails
+    def __init__(self, to, subject, body, fail_silently=True):
+        self.to = to
+        self.subject = subject
+        self.body = body
         self.fail_silently = fail_silently
         threading.Thread.__init__(self)
 
+    def render(self, tmpl, mail):
+        try:
+            m = Member.objects.get(email=mail)
+            ctxt = Context({"name": m.name.capitalize(),
+                            "family_name": m.family_name.capitalize(),
+                            "email": mail,
+                            "phone": m.phone,
+                            "username": m.username,
+                            })
+        except Exception as e:
+            print(e, mail)
+            ctxt = Context()
+        return tmpl.render(ctxt)
+
     def run(self):
-        send_mass_mail(self.mails, fail_silently=self.fail_silently)
+        tmpl = Template(self.body)
+        mails = ((self.subject, self.render(tmpl, mail),
+                  django.conf.settings.DEFAULT_FROM_EMAIL, [mail])
+                 for mail in self.to)
+        import pprint
+        pprint.pprint(list(mails))
+        send_mass_mail(mails, fail_silently=self.fail_silently)
 
 
 class EmailForm(forms.Form):
@@ -25,6 +50,4 @@ class EmailForm(forms.Form):
               for m in m.split(',') if m}
         subject = self.cleaned_data["subject"].strip()
         body = self.cleaned_data["body"]
-        mails = ((subject, body, django.conf.settings.DEFAULT_FROM_EMAIL, [m])
-                 for m in to)
-        MassMailThread(mails, fail_silently=True).start()
+        MassMailThread(to, subject, body, fail_silently=True).start()
