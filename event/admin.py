534a0cd1273171844884a716c152ef6268e989fa
Moez Bouhlel <bmoez.j@gmail.com>
2016-11-10 15:32:16 +0100

    update event register admin panel

--- event/admin.py
+++ event/admin.py
@@ -5,5 +5,7 @@ from db.models import Member, Event
 
 @admin.register(Register)
 class RegisterAdmin(admin.ModelAdmin):
-    list_display = ('event', 'member')
+    list_display = ('event', 'member', 'get_member_email',
+                    'get_member_university', 'get_member_education',
+                    'get_member_year')
     list_filter = ('event', 'member')
--- event/models.py
+++ event/models.py
@@ -11,3 +11,23 @@ class Register(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.event, self.member)
+
+    def get_member_email(self):
+        return self.member.email
+    get_member_email.short_description = 'email'
+
+    def get_member_university(self):
+        return self.member.inscription_set.all().\
+            order_by('-session')[0].university
+    get_member_university.short_description = 'University'
+    get_member_university.admin_order_field = 'member__inscription__university'
+
+    def get_member_education(self):
+        return self.member.inscription_set.all().\
+            order_by('-session')[0].education
+    get_member_education.short_description = 'education'
+    get_member_education.admin_order_field = 'member__inscription__education'
+
+    def get_member_year(self):
+        return self.member.inscription_set.all().order_by('-session')[0].year
+    get_member_year.short_description = 'year'
