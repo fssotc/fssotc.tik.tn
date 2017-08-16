577c8a9534114493a34602398da0fe32c94e07d5
Moez Bouhlel <bmoez.j@gmail.com>
2017-08-16 13:44:52 +0100

    update migrations

--- wordpress/models.py
+++ wordpress/models.py
@@ -34,8 +34,8 @@ class WordPressIDs(models.Model):
 
 
 class Category(WordPressIDs, DateTracking, models.Model):
-    name = models.CharField(max_length=1000, blank=False, null=False)
-    slug = models.SlugField(max_length=1000, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    slug = models.SlugField(max_length=255, blank=False, null=False, unique=True)
     description = models.TextField(blank=True, null=False)
     post_count = models.IntegerField(blank=False, null=False)
     parent_wp_id = models.IntegerField(blank=True, null=True)
@@ -51,8 +51,8 @@ class Category(WordPressIDs, DateTracking, models.Model):
 
 
 class Tag(WordPressIDs, DateTracking, models.Model):
-    name = models.CharField(max_length=1000, blank=False, null=False)
-    slug = models.SlugField(max_length=1000, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    slug = models.SlugField(max_length=255, blank=False, null=False, unique=True)
     description = models.TextField(blank=True, null=False)
     post_count = models.IntegerField(blank=False, null=False)
 
@@ -65,12 +65,12 @@ class Tag(WordPressIDs, DateTracking, models.Model):
 
 class Author(WordPressIDs, DateTracking, models.Model):
     login = models.CharField(max_length=255, blank=False, null=False)
-    email = models.CharField(max_length=1000, blank=False, null=False)
-    name = models.CharField(max_length=1000, blank=False, null=False)
-    nice_name = models.CharField(max_length=1000, blank=False, null=False)
-    url = models.CharField(max_length=1000, blank=False, null=False)
-    avatar_url = models.CharField(max_length=1000, blank=False, null=False)
-    profile_url = models.CharField(max_length=1000, blank=False, null=False)
+    email = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    nice_name = models.CharField(max_length=255, blank=False, null=False)
+    url = models.CharField(max_length=255, blank=False, null=False)
+    avatar_url = models.CharField(max_length=255, blank=False, null=False)
+    profile_url = models.CharField(max_length=255, blank=False, null=False)
 
     def __unicode__(self):
         return self.name
@@ -80,9 +80,9 @@ class Author(WordPressIDs, DateTracking, models.Model):
 
 
 class Media(WordPressIDs, DateTracking, models.Model):
-    url = models.CharField(max_length=1000, blank=False, null=False,
+    url = models.CharField(max_length=255, blank=False, null=False,
                            help_text=_("The full URL to the media file"))
-    guid = models.CharField(max_length=1000, blank=True, null=True, db_index=True)
+    guid = models.CharField(max_length=255, blank=True, null=True, db_index=True)
     uploaded_date = models.DateTimeField(blank=False, null=False)
     post_ID = models.IntegerField(blank=True, null=True,
                                   help_text=_("ID of the post this media is attached to"))
@@ -110,25 +110,25 @@ class Post(WordPressIDs, DateTracking, models.Model):
     modified = models.DateTimeField(blank=False, null=False,
                                     help_text=_("The post's most recent update time"))
     title = models.TextField(blank=True, null=True)
-    url = models.CharField(max_length=1000, blank=False, null=False,
+    url = models.CharField(max_length=255, blank=False, null=False,
                            help_text=_("The full permalink URL to the post"))
-    short_url = models.CharField(max_length=1000, blank=False, null=False,
+    short_url = models.CharField(max_length=255, blank=False, null=False,
                                  help_text=_("The wp.me short URL"))
     content = models.TextField(blank=True, null=True)
     excerpt = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=200, blank=True, null=True, db_index=True)
-    guid = models.CharField(max_length=1000, blank=True, null=True, db_index=True)
+    guid = models.CharField(max_length=255, blank=True, null=True, db_index=True)
     status = models.CharField(max_length=20, blank=True, null=True)
     sticky = models.BooleanField(default=False,
                                  help_text=_("Show this post at the top of the chronological list, even if old."))
-    password = models.CharField(max_length=1000, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
     parent = JSONField(load_kwargs={'object_pairs_hook': collections.OrderedDict}, blank=True, null=True)
     post_type = models.CharField(max_length=20, blank=True, null=True)
     likes_enabled = models.NullBooleanField()
     sharing_enabled = models.NullBooleanField()
     like_count = models.IntegerField(blank=True, null=True)
-    global_ID = models.CharField(max_length=1000)
-    featured_image = models.CharField(max_length=1000)
+    global_ID = models.CharField(max_length=255)
+    featured_image = models.CharField(max_length=255)
     post_thumbnail = JSONField(blank=True, null=True, load_kwargs={'object_pairs_hook': collections.OrderedDict})
     attachments = models.ManyToManyField("Media", blank=True)
     format = models.CharField(max_length=20)
